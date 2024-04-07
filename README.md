7cb1faa1a51a63e949028eb6113356139b8a2f6c
gursewak <gursewak@gursewaks-MBP.lan>
2024-04-07 17:43:14 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# sponsorpage
+
+This template should help get you started developing with TailwindCSS.
+
+## Project Setup
+
+```sh
+npm install
+```
+
+### Compile and Hot-Reload for Development
+
+```sh
+npm run tailwind
+```
+
+ 
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: '1rem',
+          sm: '2rem',
+          lg: '3rem',
+          xl: '4rem',
+          '2xl': '5rem',
+        },
+      },
+    },
+  },
+  plugins: [],
+}
+
